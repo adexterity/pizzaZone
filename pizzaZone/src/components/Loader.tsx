8b9066b5d5dfe32c636dca2cfb91c4955f585450
adexterity <opensesame4me@gmail.com>
2024-05-22 01:27:29 +0100

    useCycle hook in framer-motion

--- pizzaZone/src/components/Loader.tsx
+++ pizzaZone/src/components/Loader.tsx
@@ -1,30 +1,41 @@
-import { motion } from "framer-motion";
+import { motion, useCycle } from "framer-motion";
 
 const loaderVariants = {
-  animateOne: {
-    y: [-10, 10],
-    transition: { ease: "easeOut", repeat: Infinity, repeatType: "reverse", duration: .5 },
+  animationOne: {
+    x: [-20, 20],
+    y: [0, -30],
+    transition: { x:{
+      duration: 0.5, repeat: Infinity, repeatType: "reverse", 
+    }, y: {ease: "easeOut", duration: .25, repeat: Infinity, repeatType: "reverse"} },
   },
-  animateTwo: {
-    y: [10, -10],
+  animationTwo: {
+    x: [10, -10],
     transition: { ease: "easeOut", repeat: Infinity, repeatType: "reverse", duration: .5 },
-  },
-};
+  }
+}
 
 const Loader = () => {
+
+  const [animation, cycleAnimation] = useCycle('animationOne', 'animationTwo')
   return (
     <>
       <div>
         <motion.div
           variants={loaderVariants}
-          animate="animateOne"
+          animate= {animation}
           className="loader"
+        ></motion.div>
+        {/* <motion.div
+          variants={loaderVariants}
+          animate="animationTwo"
+          className="loader spacing"
         ></motion.div>
         <motion.div
           variants={loaderVariants}
-          animate="animateTwo"
+          animate="animationThree"
           className="loader"
-        ></motion.div>
+        ></motion.div> */}
+        <div onClick={()=>cycleAnimation()}>change animation</div>
       </div>
     </>
   );
